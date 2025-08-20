61cb75ffd56ccf6554a56f78b5830f9496cd779b
Sneha-Amballa <sneha.amballa0804@gmail.com>
2025-08-20 13:48:19 +0530

    Removed test file

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema({
     description: {type:String, required: true},
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
-    //imageUrl: {type: String} 
+    imageUrl: {type: String} 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
